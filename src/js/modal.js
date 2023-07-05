34226ff5feb9e4bca68e07c09cb7117be5d89ff7
Nemec980 <131163877+dyatskovskiy@users.noreply.github.com>
2023-07-05 17:22:11 +0300

    modal fix/done

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,3 +1,60 @@
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
+    const modals = [
+      "data-first-modal",
+      "data-second-modal",
+      "data-third-modal",
+    ];
+
+
+    modals.forEach((element) => {
+      const openModalSelector = element + "-open";
+      const closeModalSelector = element + "-close";
+
+      const openModalBtns = document.querySelectorAll(`[${openModalSelector}]`);
+      const closeModalBtns = document.querySelectorAll(
+        `[${closeModalSelector}]`
+      );
+      const modal = document.querySelector(`[${element}]`);
+
+      if (!modal) logModalError("Can`t find Modal with attribute " + modal);
+      if (openModalBtns.length === 0)
+        logModalError(
+          "Can`t find Open modal button with attribute " + openModalSelector
+        );
+      if (closeModalBtns.length === 0)
+        logModalError(
+          "Can`t find Close modal button with attribute " + closeModalSelector
+        );
+      if (!modal || openModalBtns.length === 0 || closeModalBtns.length === 0)
+        return;
+
+      openModalBtns.forEach((openBtn) =>
+        openBtn.addEventListener("click", toggleModal)
+      );
+      closeModalBtns.forEach((closeBtn) =>
+        closeBtn.addEventListener("click", toggleModal)
+      );
+
+      function toggleModal() {
+        document.body.classList.toggle("modal-open");
+        modal.classList.toggle("is-hidden");
+      }
+    });
+  },
+  false
+);
+
+function logModalError(text) {
+  const styles = "color: #bada55";
+  console.log("%c" + text, styles);
+}
+
+
+
+// HEАКТУАЛЬНИЙ СКРИПТ ======================
+
 // const buttonMenu = document.querySelector('.js-btn-hide');
 // const onHideBtn = document.querySelector('.js-hide-on');
 // const offHideBtn = document.querySelector('.js-hide-off');
@@ -21,18 +78,18 @@
 //     if (!e.matches) return;
 //     mobileMenu.classList.remove('is-open');
 // });
+// ============================
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector("[data-modal-open]"),
+//     closeModalBtn: document.querySelector("[data-modal-close]"),
+//     modal: document.querySelector("[data-modal]"),
+//   };
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
+//   refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
+//   function toggleModal() {
+//     refs.modal.classList.toggle("is-hidden");
+//   }
+// })();
